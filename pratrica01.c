8902c865aa787a9eefc6916717e1d0c74fd66442
pedrohraulino <pedrohraulino@gmail.com>
2023-06-28 17:35:19 -0300

    att

--- pratrica01.c
+++ pratrica01.c
@@ -1,7 +1,13 @@
- #include <stdio.h>
+#include <stdio.h>
 
-int main(void){
-    int x=2;
-    int *p=&x;
+int main(void) {
+    float var = 2.5;
+    float *ponteiro;
+
+    ponteiro = &var;
+
+    printf("var = %f \n", var);
+    printf("ponteiro = %p \n", ponteiro);
+
     return 0;
 }
