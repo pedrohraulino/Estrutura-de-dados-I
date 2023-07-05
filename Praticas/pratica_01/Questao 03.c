ec38e5be7b31154a89802ba6c08f24401a20bd03
pedrohraulino <pedrohraulino@gmail.com>
2023-07-05 18:25:59 -0300

    Questão 03

---
+++ Praticas/pratica_01/Questao 03.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+
+    int tamanho, linhas, linha, colunas, coluna;
+    
+    printf("digita o tamanho\n");
+    scanf("%d", &tamanho);
+    
+    linhas = tamanho;
+    colunas = tamanho;
+    int **matriz = (int**) malloc(linhas * sizeof(int*));
+
+    if (matriz == NULL){
+       printf("erro ao alocar memoria \n");
+       exit(1);
+    }
+
+    else{
+        printf("alocacao realizda com sucesso \n");
+    }
+
+    printf("digite os valores do vetor \n");
+
+    int contador;
+
+    for (contador = 0; contador < linhas; contador++){
+        matriz[contador] = (int*) malloc (colunas*sizeof(int));
+    }
+
+    for (linha = 0; linha < linhas; linha++){
+        for (coluna = 0; coluna < colunas; coluna++){
+           matriz[linha][coluna] = rand() % 101;
+        }
+    }
+
+    for (coluna = 0; coluna < colunas; coluna++){
+        for (linha = 0; linha < linhas; linha++){
+            printf("%d ", matriz[linha][coluna]);
+        }
+        printf("\n");
+    }
+   
+    free(matriz);
+
+    return 0;
+}
