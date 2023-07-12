8746676cf1563858606be2cfb22050abf87dcca7
pedrohraulino <pedrohraulino@gmail.com>
2023-07-12 16:48:55 -0300

    Alocação dinamica com ponteiros

---
+++ Struct/Aloção dinamica/struct Aluno (Malloc).c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct disciplina
+{
+    char nome[20];
+    float nota;
+
+}Disciplina;
+
+
+typedef struct aluno
+{
+    char nome[50];
+    int idade;
+    int matricula;
+    Disciplina *disciplina;
+
+} Aluno;
+
+
+int main(void)
+{
+    Aluno * estruturaAluno =  (Aluno *) malloc (sizeof(Aluno));
+    
+    if (estruturaAluno  == NULL){
+        printf("Não foi possivel alocar a memoria");
+        exit(1);
+    }
+    
+    estruturaAluno->disciplina = (Disciplina*) malloc (1*sizeof(Disciplina));
+     if (estruturaAluno->disciplina == NULL){
+        printf("Não foi possivel alocar a memoria");
+        exit(1);
+    }
+
+    printf("Digite o nome do aluno: \n");
+    scanf(" %[^\n]", estruturaAluno->nome);
+
+    printf("Digite a idade do aluno: \n");
+    scanf(" %d", &estruturaAluno->idade);
+
+    printf("Digite a matricula do aluno: \n");
+    scanf(" %d", &estruturaAluno->matricula);
+
+    printf("Digite a disciplina: \n");
+    scanf(" %[^\n]", &estruturaAluno->disciplina->nome);
+
+    printf("Digite a nota: \n");
+    scanf(" %f", &estruturaAluno->disciplina->nota);
+
+    printf("O nome do aluno de matricula %d é %s. Ele tem %d anos.%s esta matriculado na disciplina: %s com nota: %.2f\n", estruturaAluno->matricula, estruturaAluno->nome, estruturaAluno->idade, estruturaAluno->nome, estruturaAluno->disciplina->nome, estruturaAluno->disciplina->nota);
+
+    free(estruturaAluno->disciplina);
+    free(estruturaAluno);
+
+    return 0;
+}
