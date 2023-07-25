8a29ca84dd44bc6dd36016ab29e0dee3b6b572d0
pedrohraulino <pedrohraulino@gmail.com>
2023-07-25 08:44:47 -0300

    adicao enun

---
+++ Enum/enun.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+
+typedef enum boolean { 
+   false, true
+}bool;
+
+
+
+int main() { 
+    bool b; 
+    printf("Digite O ou 1\n");
+    scanf("%d", &b);
+    printf("O valor infomrado: %d", b);
+
+    if (b==true)
+    {
+        printf(" é verdadeiro");
+    }
+
+    else{
+        printf(" é falso");
+    }
+    
+    return 0;
+}
